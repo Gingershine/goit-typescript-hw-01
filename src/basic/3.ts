595f60f2f1e75ee73002ace4fa7487e0d5c6c606
Gingershine <142210638+Gingershine@users.noreply.github.com>
2024-05-10 16:31:47 +0300

    add

---
+++ src/basic/3.ts
@@ -0,0 +1,4 @@
+
+let value: string | number;
+
+let state: 'enable' | 'disable';
---
+++ src/generics/4.ts
@@ -0,0 +1,17 @@
+
+type UserData = {
+  name: string;
+  surname: string;
+  email: string;
+  password: string;
+}
+
+function createOrUpdateUser(initialValues: Partial<UserData>) {
+  return  {...initialValues}
+
+}
+
+createOrUpdateUser({ 
+  email: 'user@mail.com', 
+  password: 'password123' 
+});
