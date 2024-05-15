011109a05234de56e4229ae37a4bc9412594d9cd
Gingershine <142210638+Gingershine@users.noreply.github.com>
2024-05-15 12:40:42 +0300

    fixing

--- src/generics/1.ts
+++ src/generics/1.ts
@@ -1,10 +1,10 @@
 
-import axios from 'axios';
+import axios, { AxiosResponse } from 'axios';
 
 async function fetchData<T>(url: string): Promise<T> {
     try {
-    const response = await axios.get(url);
-    return response.data as T;
+    const response: AxiosResponse<T> = await axios.get<T>(url);
+    return response.data;
   } catch (error) {
     throw new Error(`Error fetching from ${url}: ${error}`);
   }
